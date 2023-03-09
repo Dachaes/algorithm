89c842227bfb663768c31e188ed28e9878eee235
Dachaes <no3yho2@naver.com>
2023-03-09 12:28:51 +0900

    230309

--- 01. 그리디/01. 이것이 코딩 테스트다 - 나동빈/02. 기출 문제/02. 곱하기 혹은 더하기 (p. 312).py
+++ 01. 그리디/01. 이것이 코딩 테스트다 - 나동빈/02. 기출 문제/02. 곱하기 혹은 더하기 (p. 312).py
@@ -6,6 +6,8 @@ number = list(map(int, input()))
 res = number[0]
 for i in range(1, len(number)):
     if res == 0 or number[i] == 0:
+        continue
+    elif number[i] == 1:
         res += number[i]
     else:
         res *= number[i]
