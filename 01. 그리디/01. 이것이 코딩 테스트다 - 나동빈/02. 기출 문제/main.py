f6c0cd609547a0135f89e3b29dcaf5e732148bf3
Dachaes <no3yho2@naver.com>
2023-03-09 12:25:53 +0900

    230309

---
+++ 01. 그리디/01. 이것이 코딩 테스트다 - 나동빈/02. 기출 문제/main.py
@@ -0,0 +1,15 @@
+# 03. 문자열 뒤집기 (p. 313)
+
+# input
+numbers = list(map(int, input()))
+
+# 리스트 내의 연속된 숫자가 몇 묶음 있나?
+previous_number = numbers[0]
+group_of_num = 1
+for number in numbers:
+    if previous_number ^ number == 1:
+        group_of_num += 1
+    previous_number = number
+
+res = group_of_num
+print(res)
