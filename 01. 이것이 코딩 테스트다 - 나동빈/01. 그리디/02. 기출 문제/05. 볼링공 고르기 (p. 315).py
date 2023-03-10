4f68c31dfc7eaf69dea96e1706071576cc779425
Dachaes <no3yho2@naver.com>
2023-03-10 13:09:49 +0900

    230310

--- 01. 이것이 코딩 테스트다 - 나동빈/01. 그리디/02. 기출 문제/05. 볼링공 고르기 (p. 315).py
+++ 01. 이것이 코딩 테스트다 - 나동빈/01. 그리디/02. 기출 문제/05. 볼링공 고르기 (p. 315).py
@@ -6,9 +6,7 @@ balls = list(map(int, input().split()))
 
 res = 0
 for i in range(N):
-    ball1 = balls[i]
     for j in range(N):
-        ball2 = balls[j]
         if i < j and balls[i] != balls[j]:
             res += 1
 print(res)
