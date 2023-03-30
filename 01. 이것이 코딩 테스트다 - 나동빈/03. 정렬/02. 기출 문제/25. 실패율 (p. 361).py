abe0f4a12e872b921d6677e464027252fceb7004
Dachaes <no3yho2@naver.com>
2023-03-30 12:49:25 +0900

    230330

---
+++ 01. 이것이 코딩 테스트다 - 나동빈/03. 정렬/02. 기출 문제/25. 실패율 (p. 361).py
@@ -0,0 +1,34 @@
+# 25. 실패율 (p. 361)
+# https://programmers.co.kr/learn/courses/30/lessons/42889
+
+def solution(N, stages):
+    len_stages = len(stages)
+    users = []
+
+    for stage in range(1, N + 1):
+        cnt = stages.count(stage)
+        if cnt == 0:
+            users.append([stage, 0])
+        else:
+            users.append([stage, cnt / len_stages])
+        len_stages -= cnt
+
+    users.sort(key=lambda x: (-x[1], x[0]))
+
+    answer = []
+    for user in users:
+        answer.append(user[0])
+
+    return answer
+
+# Input
+N1 = 5
+STAGES1 = [2, 1, 2, 6, 2, 4, 3, 3]
+# [3, 4, 2, 1, 5]
+
+N2 = 4
+STAGES2 = [4, 4, 4, 4, 4]
+# [4, 1, 2, 3]
+
+# Output
+print(solution(N1, STAGES1))
---
+++ 01. 이것이 코딩 테스트다 - 나동빈/03. 정렬/02. 기출 문제/26. 카드 정렬하기 (p. 363).py
@@ -0,0 +1,3 @@
+# 26. 카드 정렬하기 (p. 363)
+# https://www.acmicpc.net/problem/1715
+
