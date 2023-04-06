b8b4939feb1f5db99edd051358fbf4760da65cb5
Dachaes <no3yho2@naver.com>
2023-04-06 14:02:13 +0900

    230406

--- 01. 이것이 코딩 테스트다 - 나동빈/04. 다이나믹 프로그래밍/01. 실전 문제/04. 효율적인 화폐 구성 (p. 226).py
+++ 01. 이것이 코딩 테스트다 - 나동빈/04. 다이나믹 프로그래밍/01. 실전 문제/04. 효율적인 화폐 구성 (p. 226).py
@@ -1,11 +1,30 @@
 # 04. 효율적인 화폐 구성 (p. 226)
 
 # Input
-N = int(input())
+num, target = map(int, input().split())
+moneys = []
+for _ in range(num):
+    moneys.append(int(input()))
+moneys.sort()
 
-cover = [-1, 1, 3]
-for i in range(3, N + 1):
-    cover.append(cover[i - 2] * 2 + cover[i - 1])
+sum_price = [0]
+for i in range(1, target + 1):
+    # 1. 목표값이 가지고 있는 화폐의 단위보다 작을 경우, 불가능 -> -1
+    if i < moneys[0]:
+        sum_price.append(-1)
+    # 2. 목표값이 가지고 있는 화폐의 단위랑 동일할 경우, 화폐 단위 한개로 만들 수 있음 -> 1
+    elif i in moneys:
+        sum_price.append(1)
+    # 3. 목표값이 가지고 있는 화폐의 합으로 계산해야 할 경우,
+    # 이전에 계산했던 값(이전 누적값) + 가지고 있는 화폐 단위(1개)로 계산이 가능한지 확인
+    else:
+        temp = -1
+        for money in moneys:
+            if i - money > 1 and sum_price[i - money] != -1:
+                if temp == -1:
+                    temp = sum_price[i - money] + 1
+                else:
+                    temp = min(temp, sum_price[i - money] + 1)
+        sum_price.append(temp)
 
-res = cover[N] % 796796
-print(res)
+print(sum_price[target])
