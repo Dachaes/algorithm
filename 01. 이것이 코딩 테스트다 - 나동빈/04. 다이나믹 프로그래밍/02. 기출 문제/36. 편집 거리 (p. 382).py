d8c5a44f7e77f12240896a43bdcc308e80f5b680
Dachaes <no3yho2@naver.com>
2023-04-16 12:05:14 +0900

    230416

--- 01. 이것이 코딩 테스트다 - 나동빈/04. 다이나믹 프로그래밍/02. 기출 문제/36. 편집 거리 (p. 382).py
+++ 01. 이것이 코딩 테스트다 - 나동빈/04. 다이나믹 프로그래밍/02. 기출 문제/36. 편집 거리 (p. 382).py
@@ -2,5 +2,18 @@
 
 # Input
 word_A, word_B = input(), input()
+len_A, len_B = len(word_A), len(word_B)
 
-print(word_A, word_B)
+edit_distance = [[0] * (len_B + 1) for _ in range(len_A + 1)]
+for i in range(len_A + 1):
+    for j in range(len_B + 1):
+        if i == 0:
+            edit_distance[0][j] = j
+        elif j == 0:
+            edit_distance[i][0] = i
+        elif word_A[i - 1] == word_B[j - 1]:
+            edit_distance[i][j] = edit_distance[i - 1][j - 1]
+        else:
+            edit_distance[i][j] = min(edit_distance[i - 1][j - 1], edit_distance[i - 1][j], edit_distance[i][j - 1]) + 1
+
+print(edit_distance[-1][-1])
--- 01. 이것이 코딩 테스트다 - 나동빈/05. DFS-BFS/01. 실전 문제/01. 음료수 얼려 먹기 (p. 149).py
+++ 01. 이것이 코딩 테스트다 - 나동빈/05. DFS-BFS/01. 실전 문제/01. 음료수 얼려 먹기 (p. 149).py
@@ -1,7 +1,54 @@
 # 01. 음료수 얼려 먹기 (p. 149)
-from collections import deque
 
 # Input
+n, m = map(int, input().split())
+ice = []
+for i in range(n):
+    ice.append(list(map(int, input())))
 
-queue = deque([3])
-print(queue)
+def same_ice(row, col):
+    if ice[row][col] == 0:
+        ice[row][col] = 1
+        if row + 1 < n:
+            same_ice(row + 1, col)
+        if col + 1 < m:
+            same_ice(row, col + 1)
+
+res = 0
+for i in range(n):
+    for j in range(m):
+        if ice[i][j] == 0:
+            ice[i][j] = 1
+            res += 1
+            if i + 1 < n:
+                same_ice(i + 1, j)
+            if j + 1 < m:
+                same_ice(i, j + 1)
+
+
+print(res)
+
+# 4 5
+# 00110
+# 00011
+# 11111
+# 00000
+# res = 3
+
+# 15 14
+# 00000111100000
+# 11111101111110
+# 11011101101110
+# 11011101100000
+# 11011111111111
+# 11011111111100
+# 11000000011111
+# 01111111111111
+# 00000000011111
+# 01111111111000
+# 00011111111000
+# 00000001111000
+# 11111111110011
+# 11100011111111
+# 11100011111111
+# res = 8
