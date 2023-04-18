e7b864fa8e181c5f396eda6b2c254aa1366ace7b
Dachaes <no3yho2@naver.com>
2023-04-18 13:07:27 +0900

    230418

---
+++ 01. 이것이 코딩 테스트다 - 나동빈/05. DFS-BFS/01. 실전 문제/02-2. 미로 탈출 (p. 152).py
@@ -0,0 +1,42 @@
+# 02. 미로 탈출 (p. 152)
+from collections import deque
+
+# Input
+n, m = map(int, input().split())
+maze = []
+for i in range(n):
+    maze.append(list(map(int, input())))
+
+maze_deq = deque([(0, 0)])
+dx = [0, 0, 1, -1]
+dy = [1, -1, 0, 0]
+
+while maze_deq:
+    x, y = maze_deq[0][0], maze_deq[0][1]
+    seq = maze[x][y]
+
+    for i in range(4):
+        next_x, next_y = x + dx[i], y + dy[i]
+        if 0 <= next_x < n and 0 <= next_y < m:
+            if maze[next_x][next_y] == 1 and (next_x, next_y) != (0, 0):
+                maze_deq.append((next_x, next_y))
+                maze[next_x][next_y] = seq + 1
+    maze_deq.popleft()
+
+print(maze[n - 1][m - 1])
+
+# 5 6
+# 101010
+# 111111
+# 000001
+# 111111
+# 111111
+# res = 10
+
+# 5 6
+# 111111
+# 111111
+# 111111
+# 111111
+# 111111
+# res = 10
