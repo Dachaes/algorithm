1cd4b051901f7ec5ddec26846f795d98d031a398
Dachae <no3yho2@naver.com>
2023-05-26 13:26:20 +0900

    230526

--- 01. 이것이 코딩 테스트다 - 나동빈/05. DFS-BFS/02. 기출 문제/18-2. 괄호 변환 (p. 346) - list 사용.py
+++ 01. 이것이 코딩 테스트다 - 나동빈/05. DFS-BFS/02. 기출 문제/18-2. 괄호 변환 (p. 346) - list 사용.py
@@ -2,10 +2,12 @@
 # https://programmers.co.kr/learn/courses/30/lessons/60058
 from collections import deque
 
-def is_correct(deq):
+
+# '올바른 괄호 문자열'인지 확인하는 함수
+def is_correct(string):
     num = 0
-    for i in range(len(deq)):
-        char = deq[i]
+    for i in range(len(string)):
+        char = string[i]
         if char == '(':
             num += 1
         elif char == ')':
@@ -15,37 +17,36 @@ def is_correct(deq):
     return True
 
 
-def correct_u(deq):
-    deq.popleft()
-    deq.pop()
-    for _ in range(len(deq)):
-        char = deq.popleft()
-        if char == '(':
-            deq.append(')')
-        elif char == ')':
-            deq.append('(')
-    return deq
+# 문자열 u을 '올바른 괄호 문자열'으로 교정하는 함수
+def correct_u(string):
+    new_str = ''
+    for i in range(len(string)):
+        char = string[i]
+        if char == '(' and i != 0 and i != len(string) - 1:
+            new_str += ')'
+        elif char == ')' and i != 0 and i != len(string) - 1:
+            new_str += '('
+    return new_str
 
 
 def solution(p):
     global answer
-    # print(f"입력 문자열: {p}")
     # 1. 입력이 빈 문자열일 경우, 빈 문자열을 반환한다.
     if not p:
         return p
     
     # 2. 입력받은 문자열을 두 '균형잡힌 괄호 문자열' u와 v로 분리한다.
-    u = deque()
-    v = deque(p)
+    u = ''
+    v = ''
 
     separated = False
     left, right = 0, 0
-    for _ in range(len(p)):
-        char = v.popleft()
+    for i in range(len(p)):
+        char = p[i]
         if not separated:
-            u.append(char)
+            u += char
         else:
-            v.append(char)
+            v += char
 
         if char == '(':
             left += 1
@@ -53,17 +54,14 @@ def solution(p):
             right += 1
         if left == right:
             separated = True
-    # print(f"u: {''.join(u)}, v: {''.join(v)}")
 
     # 3. 수행한 결과 문자열을 u에 이어붙인 후 반환한다.
     # 3-1. 문자열 u가 '올바른 괄호 문자열'이라면 v에 대해 1단계부터 다시 수행한다.
     if is_correct(u):
-        # print(f"-> u는 올바름")
-        answer = answer + ''.join(u) + solution(''.join(v))
+        answer = answer + u + solution(v)
     # 4. 문자열 u가 '올바른 괄호 문자열'이 아니면 아래의 과정을 수행한다.
     else:
-        # print(f"-> u는 올바르지 않음")
-        answer = answer + '(' + solution(''.join(v)) + ')' + ''.join(correct_u(u))
+        answer = answer + '(' + solution(v) + ')' + ''.join(correct_u(u))
 
     return answer
 
