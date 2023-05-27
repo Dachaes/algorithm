aa5237586572734827891ada9030625d638c71bf
Dachae <no3yho2@naver.com>
2023-05-27 16:00:59 +0900

    230527

--- 01. 이것이 코딩 테스트다 - 나동빈/05. DFS-BFS/02. 기출 문제/19-2. 연산자 끼워 넣기 (p.349).py
+++ 01. 이것이 코딩 테스트다 - 나동빈/05. DFS-BFS/02. 기출 문제/19-2. 연산자 끼워 넣기 (p.349).py
@@ -7,13 +7,6 @@ num_of_numbers = int(input())
 numbers = list(map(int, sys.stdin.readline().split()))
 operators = list(map(int, sys.stdin.readline().split()))
 
-# 사용할 수 있는 연산자를 확인하는 함수
-def availabe_operators(oper_list):
-    next_oper = [False, False, False, False]
-    for i in range(4):
-        if oper_list[i] > 0:
-            next_oper[i] = True
-    return next_oper
 
 # 주어진 값을 계산하는 함수
 def calculate(cal_num1, cal_num2, oper_type):
@@ -27,7 +20,7 @@ def calculate(cal_num1, cal_num2, oper_type):
         return int(cal_num1 / cal_num2)
 
 
-def solution(depth, res, oper_list):
+def solution(depth, res):
     # 종료 조건 : 모든 연산자를 사용해서 마지막 연산까지 완료하면 종료한다.
     if depth == num_of_numbers:
         global answer
@@ -45,21 +38,23 @@ def solution(depth, res, oper_list):
         num1 = res
     num2 = numbers[depth]
 
+    global operators
     # 2. 사용 가능한 연산자 리스트(oper_list)를 확인하여 현재 depth(ex. depth 1)에서 할 수 있는 계산을 한다.
     for i in range(4):
-        if oper_list[i]:
+        if operators[i]:
             res = calculate(num1, num2, i)
             # 3-1. 사용한 연산자를 전체 연산자 리스트(operators)에서 제외하고 더 깊은 depth(ex. depth 2)로 들어간다.
             # ex. depth 1 -> depth 2-1
             operators[i] -= 1
-            solution(depth + 1, res, operators)
+            solution(depth + 1, res)
             # 3-2. 깊은 depth 계산을 완료하고, 원래의 depth(ex. depth 1)로 되돌아 오면서
             # 다음 차례의 depth(ex. depth 2)를 위해 썼던 연산자를 되돌려놓는다.
             # ex. depth 1 -> depth 2-1 -> depth 3(끝) -> depth 2-2
             operators[i] += 1
 
+
 answer = []
-solution(1, 0, availabe_operators(operators))
+solution(1, 0)
 
 print(answer[0])
 print(answer[1])
