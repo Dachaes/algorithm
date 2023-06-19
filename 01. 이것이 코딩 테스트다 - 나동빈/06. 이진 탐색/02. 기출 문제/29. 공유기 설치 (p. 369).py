e816eca9b90e4306f0e9c119327a03aeeca117ec
Dachae <no3yho2@naver.com>
2023-06-19 16:40:10 +0900

    230619

--- 01. 이것이 코딩 테스트다 - 나동빈/06. 이진 탐색/02. 기출 문제/29. 공유기 설치 (p. 369).py
+++ 01. 이것이 코딩 테스트다 - 나동빈/06. 이진 탐색/02. 기출 문제/29. 공유기 설치 (p. 369).py
@@ -9,23 +9,37 @@ for _ in range(n):
     houses.append(int(sys.stdin.readline()))
 houses.sort()
 
-houses_with_router = []
 
-def install_router(start, end):
+def find_optimal_gap(start, end):
+    gap = -1
+    while start <= end:
+        mid = (start + end) // 2
+        num_routers = possible_routers(mid)
+        if num_routers < c:
+            end = mid - 1
+        else:
+            start = mid + 1
+            gap = mid
+
+    return gap
 
 
-    return
+def possible_routers(gap):
+    target = houses[0] + gap
+    count = 1
+    for house in houses:
+        if house >= target:
+            target = house + gap
+            count += 1
 
+    return count
+
+
+min_gap, max_gap = 1, houses[-1] - houses[0]
 
 if c == 2:
-    answer = n - 2
+    answer = max_gap
 else:
-    for i in range(1, c + 1):
-        if i == 1:
-            houses_with_router.append(houses[0])
-        elif i == 2:
-            houses_with_router.append(houses[-1])
-        else:
-            install_router()
+    answer = find_optimal_gap(min_gap, max_gap)
 
 print(answer)
